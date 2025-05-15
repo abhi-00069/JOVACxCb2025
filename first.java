b7f26d06949ee5cf96433b633ce783e8c637962f
abhi-00069 <abhisheksharma41324@gmail.com>
2025-05-15 23:15:01 +0530

    Create first.java

---
+++ first.java
@@ -0,0 +1,137 @@
+import java.sql.SQLOutput;
+import java.util.*;
+
+class Main{
+/*
+    public static void main(String[] stake){
+
+        Stack<Object> stc = new Stack<Object>();
+
+        System.out.println(stc.empty());
+
+//      adding = stc.push();
+        stc.push("yo");
+        stc.push("bro");
+        stc.push("watcha");
+        stc.push("doin!!??");
+        stc.push(4);
+        stc.push("me");
+        System.out.println(stc.empty());
+
+        System.out.println(stc);
+
+//      remove = stc.pop();
+        stc.pop();
+        stc.pop();
+        System.out.println(stc);
+
+        String top = (String)stc.pop();
+        System.out.println(top);
+        System.out.println(stc);
+//      this will remove topmost element and store it
+
+
+//      just lookin = stc.peek();
+        String t2 = (String)stc.peek();
+        System.out.println(t2);
+        System.out.println(stc);
+//      this will store the topmost element but wont remove it
+
+
+
+//      Searching an item through stack
+//      TOP STARTS WITH POSITION 1
+//      searching = stc.search(OBJECT);
+        int i = stc.search("bro");
+        System.out.println(i);
+//      if shit aint in stack, it'll return -1  <----------imp
+        int j =stc.search("doin");
+        System.out.println(j);
+
+    }
+
+ */
+
+/*
+    public static void main(String[] kyu) {
+
+        Queue<Object> que = new LinkedList<Object>();
+//      we used linked list because queue is an interface and not a class
+//      we cannot create an instance of an interface
+//      to use queue, we need a class that implements queue
+//      there are two-- LinkedList and PriorityQueue
+//      we using first one
+
+
+        System.out.println(que.isEmpty());
+//      adding = que.add();
+//      adding = que.offer(); <------WAY BETTER!!!!!!!!!!
+//      ALWAYS ADDS AFTER THE TAIL
+        que.offer("wha");
+        que.offer("da");
+        que.offer("hail!!");
+        que.offer('*');
+        que.offer("music");
+        que.offer("starts");
+        System.out.println(que.isEmpty());
+        System.out.println(que);
+//      [wha, da, hail!!, *, music, starts]
+//      Head                         Tail
+
+
+//      remove = que.remove();
+//      remove = que.poll(); <-----Way better!!!!!!!!
+//      ALWAYS REMOVES THE HEAD
+        que.poll();
+        System.out.println(que);
+
+        String x = (String)que.poll();
+        System.out.println(x);
+//      this will store the head and remove it
+
+
+//      just lookin = que.element();
+//      just lookin = que.peek(); <------NOW U GET IT ;)
+//      ALWAYS LOOKS AT THE HEAD
+        String str = (String)que.peek();
+        System.out.println(str);
+//      this will store the element without removing
+
+        System.out.println(que.size());
+        System.out.println(que.contains('*'));
+
+
+    }
+ */
+
+ /*
+    public static void main(String[] pkyu){
+
+        Queue<String> pque = new PriorityQueue<String>();
+
+        pque.offer("C");
+        pque.offer("A");
+        pque.offer("F");
+        pque.offer("Z");
+        pque.offer("R");
+
+        System.out.println(pque);
+
+        pque.poll();
+
+        while(!pque.isEmpty()){
+            System.out.println(pque.poll());
+        }
+
+
+    }
+
+  */
+
+    public static void main(String[] Llist){
+
+        LinkedList<String> llst = new LinkedList<String>();
+
+    }
+
+}
